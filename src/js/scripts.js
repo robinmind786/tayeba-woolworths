7fece00e9f76449e2398254d709a7904e10f85be
Robin Mind <43693130+robinmind786@users.noreply.github.com>
2022-02-20 21:48:30 +0600

    Add files via upload

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,27 @@
+// chat application
+const chatBtn = document.querySelector('.trlive-chat-link');
+const chatApp = document.querySelector('.chating-app');
+const closeChat = document.getElementById('closeChat');
+
+chatBtn.onclick = function() {
+	chatApp.style.display = 'block';
+	this.style.display = 'none'
+}
+
+closeChat.onclick = function() {
+	chatApp.style.display = 'none';
+	chatBtn.style.display = 'block';
+}
+
+
+
+
+
+
+
+
+
+
 //scroll top the page
 function topFunction() {
 	document.body.scrollTop = 0;
